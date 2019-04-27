4b2e77f64e496001e4b786e86309cc965208550b
yaremkivb <yaremkivb@gmail.com>
2019-04-27 23:07:41 +0300

    Remove wrong folder

--- home_tasks/yaremkiv/lesson1/task1.py
+++
@@ -1,7 +0,0 @@
-name = input("What is your name? ")
-age = input("How old are you? ")
-origin = input("Where do you live? ")
-
-print("Hello " + name)
-print("Your age is " + age)
-print("You live in", origin)
